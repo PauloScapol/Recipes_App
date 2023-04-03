fc6e55a175da6c12baff45e1377f53407845da4b
LucasSilvaO <lucas.oliveiraaa20@gmail.com>
2023-04-03 17:11:24 -0300

    Realiza requisito 4 e 5

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,79 @@
+import { useEffect, useState } from 'react';
+
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    isDisabled: true,
+  });
+
+  const validateEmail = () => {
+    const magicNumber = 7;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailRegex.test(formData.email) && formData.password.length >= magicNumber) {
+      setFormData((prevState) => ({
+        ...prevState,
+        isDisabled: false,
+      }));
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        isDisabled: true,
+      }));
+    }
+  };
+
+  useEffect(() => {
+    validateEmail();
+  }, [formData.email, formData.password]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }), () => {
+      validateEmail();
+    });
+  };
+
+  const saveOnLocalStorage = () => {
+    const dataUser = {
+      email: formData.email,
+    };
+    localStorage.setItem('user', JSON.stringify(dataUser));
+  };
   return (
     <form>
       <label>
-        <input type="email" data-testid="email-input" placeholder="Email" />
+        <input
+          name="email"
+          type="email"
+          data-testid="email-input"
+          placeholder="Email"
+          onChange={ (event) => handleChange(event) }
+        />
       </label>
       <label>
-        <input type="password" data-testid="password-input" placeholder="Senha" />
+        <input
+          name="password"
+          type="password"
+          data-testid="password-input"
+          placeholder="Senha"
+          onChange={ (event) => handleChange(event) }
+        />
       </label>
       <label htmlFor="btn-enter">
-        <button type="submit" id="btn-enter" data-testid="login-submit-btn">ENTER</button>
+        <button
+          type="submit"
+          id="btn-enter"
+          data-testid="login-submit-btn"
+          disabled={ formData.isDisabled }
+          onClick={ () => saveOnLocalStorage() }
+        >
+          ENTER
+
+        </button>
       </label>
     </form>
   );
--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import App from '../App';
+import { renderWithRouter } from './helpers/renderWith';
 
 describe('Testa aplicação', () => {
+  renderWithRouter(<App />);
   const emailInput = screen.getByTestId('email-input');
   const passwordInput = screen.getByTestId('password-input');
   const submit = screen.getByTestId('login-submit-btn');
   it('Testa se os inputs estão na tela.', () => {
+    renderWithRouter(<App />);
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
     expect(submit).toBeInTheDocument();
