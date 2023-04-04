2384a6244a3ddf2a868742bde9f13718b76c78cc
Paulo Scapol <paulo.scapol2@hotmail.com>
2023-04-04 18:20:30 -0300

    Realizado o Requisito 20

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -3,8 +3,26 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Meals() {
+  const [categories, setCategories] = useState([]);
   const [meals, setMeals] = useState([]);
   const maxMeals = 12;
+  const maxMealsCategories = 5;
+
+  useEffect(() => {
+    async function getMealsCategories() {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+      const data = await response.json();
+      setCategories(data.meals.slice(0, maxMealsCategories)); // salva as 5 primeiras categorias
+    }
+
+    getMealsCategories();
+  }, []);
+
+  async function filterMealsByCategory(categoryName) {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`);
+    const data = await response.json();
+    setMeals(data.meals);
+  }
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -20,6 +38,15 @@ export default function Meals() {
   return (
     <>
       <Header title="Meals" showSearchIcon />
+      {categories.map((category) => (
+        <button
+          key={ category.strCategory }
+          data-testid={ `${category.strCategory}-category-filter` }
+          onClick={ () => filterMealsByCategory(category.strCategory) }
+        >
+          {category.strCategory}
+        </button>
+      ))}
       <div>
         {meals.map((meal, index) => (
           <div
