1e4afbf95916d4082c109d043135faf2e9016569
Paulo Scapol <paulo.scapol2@hotmail.com>
2023-04-05 17:11:02 -0300

    Realizado o Requisito 37

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+import Footer from './Footer';
+import Header from './Header';
 import '../styles/Meals.css';
 
 export default function Meals() {
