d80cb59c4077a85be8b942c4928498c30d7da0b3
LucasSilvaO <lucas.oliveiraaa20@gmail.com>
2023-04-04 23:29:31 -0300

    Realiza requisito 24

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import Profile from './pages/Profile';
 import Recipes from './pages/Recipes';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
+import RecipeDetails from './pages/RecipeDetails';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Switch>
+      <Route path="/meals/:id" component={ RecipeDetails } />
+      <Route path="/drinks/:id" component={ RecipeDetails } />
       <Route path="/done-recipes" component={ DoneRecipes } />
       <Route path="/favorite-recipes" component={ FavoriteRecipes } />
       <Route path="/profile" component={ Profile } />
-      <Route exact path="/:type" component={ Recipes } />
+      <Route path="/:type" component={ Recipes } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
