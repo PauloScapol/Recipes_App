27246f6ba0759893f5d3438ed3bb6a0cbfb0d993
Victoria Sales <contatovictoriasales@gmail.com>
2023-04-06 09:12:52 -0300

    Implementa mais testes envolvendo SearchBar

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -3,10 +3,13 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import Meals from '../components/Meals';
-// import Drinks from '../components/Drinks';
+import Drinks from '../components/Drinks';
 import mockChickenFetch from './mocks/chickenData';
 import mockSoupFetch from './mocks/soupData';
-import mockletterYFetch from './mocks/yData';
+import mockletterYFetch from './mocks/yMealsData';
+import mockEverClearFetch from './mocks/everclearData';
+import mockAbileneFetch from './mocks/abileneData';
+import mockletterYDrksFetch from './mocks/yDrinksData';
 
 const testIdSearchTopBtn = 'search-top-btn';
 const testIdSearchInput = 'search-input';
@@ -95,12 +98,12 @@ describe('Testa busca pela radio Name de SearchBar.js', () => {
   });
 });
 
-describe('Meals: Testa busca pela radio First Letter de SearchBar.js', () => {
+describe('Testa busca pela radio First Letter de SearchBar.js', () => {
   beforeEach(() => {
     global.fetch = jest.fn(mockletterYFetch);
   });
 
-  test('Se o radio selecionado for First letter, a busca na API é feita corretamente pelo primeira letra', () => {
+  test('Meals: Se o radio selecionado for First letter, a busca na API é feita corretamente pela primeira letra', () => {
     act(() => {
       render(<Meals />);
     });
@@ -120,5 +123,87 @@ describe('Meals: Testa busca pela radio First Letter de SearchBar.js', () => {
 
     expect(fetch).toBeCalled();
     expect(fetch).toBeCalledWith('https://www.themealdb.com/api/json/v1/1/search.php?f=y');
+  });
+});
+
+describe('Testa busca pela radio ingredient de SearchBar.js', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn(mockEverClearFetch);
+  });
+
+  test('Drinks: Se o radio selecionado for Ingredient, a busca na API é feita corretamente pelo ingrediente', () => {
+    act(() => {
+      render(<Drinks />);
+    });
+    const searchTopBtn = screen.getByTestId(testIdSearchTopBtn);
+    expect(searchTopBtn).toBeInTheDocument();
+    userEvent.click(searchTopBtn);
+
+    const ingredientRadio = screen.getByTestId(testIdSearchIngredientRadio);
+    userEvent.click(ingredientRadio);
+
+    const searchInput = screen.getByTestId(testIdSearchInput);
+    const ingredient = 'everclear';
+    userEvent.type(searchInput, ingredient);
+
+    const searchButton = screen.getByTestId(testIdSearchButton);
+    userEvent.click(searchButton);
+    expect(fetch).toBeCalled();
+    expect(fetch).toBeCalledWith('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=everclear');
+  });
+});
+
+describe('Testa busca pela radio Name de SearchBar.js', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn(mockAbileneFetch);
+  });
+
+  test('Drinks: Se o radio selecionado for Ingredient, a busca na API é feita corretamente pelo ingrediente', () => {
+    act(() => {
+      render(<Drinks />);
+    });
+    const searchTopBtn = screen.getByTestId(testIdSearchTopBtn);
+    expect(searchTopBtn).toBeInTheDocument();
+    userEvent.click(searchTopBtn);
+
+    const nameRadio = screen.getByTestId(testIdSearchNameRadio);
+    userEvent.click(nameRadio);
+
+    const searchInput = screen.getByTestId(testIdSearchInput);
+    const drinkName = 'abilene';
+    userEvent.type(searchInput, drinkName);
+
+    const searchButton = screen.getByTestId(testIdSearchButton);
+    userEvent.click(searchButton);
+    expect(fetch).toBeCalled();
+    expect(fetch).toBeCalledWith('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=abilene');
+  });
+});
+
+describe('Testa busca pela radio First Letter de SearchBar.js', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn(mockletterYDrksFetch);
+  });
+
+  test('Drinks: Se o radio selecionado for First letter, a busca na API é feita corretamente pela primeira letra', () => {
+    act(() => {
+      render(<Drinks />);
+    });
+    const searchTopBtn = screen.getByTestId(testIdSearchTopBtn);
+    expect(searchTopBtn).toBeInTheDocument();
+    userEvent.click(searchTopBtn);
+
+    const firstLetterRadio = screen.getByTestId(testIdSearchFirstLetterRadio);
+    userEvent.click(firstLetterRadio);
+
+    const searchInput = screen.getByTestId(testIdSearchInput);
+    const letterY = 'y';
+    userEvent.type(searchInput, letterY);
+
+    const searchButton = screen.getByTestId(testIdSearchButton);
+    userEvent.click(searchButton);
+
+    expect(fetch).toBeCalled();
+    expect(fetch).toBeCalledWith('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=y');
   });
 });
