4aeb0790ce2d3bafe04f4f3ae09c3495e801565f
Paulo Scapol <paulo.scapol2@hotmail.com>
2023-04-10 20:18:25 -0300

    Terminada a página de Receitas Prontas (Menos o Requisito 47)

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-// import shareIcon from '../images/shareIcon.svg';
 
-export default function ReceitasFeitas() {
+export default function DoneRecipes() {
   const [recipes, setRecipes] = useState([]);
-  const [filterType, setFilterType] = useState('All');
+  const [filterType, setFilterType] = useState('all');
 
   useEffect(() => {
     const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
     setRecipes(doneRecipes);
   }, []);
 
-  const handleFilter = (type) => {
-    setFilterType(type);
+  const handleFilterClick = (event) => {
+    setFilterType(event.target.name);
   };
 
   const filteredRecipes = recipes.filter((recipe) => {
-    if (filterType === 'All') {
+    if (filterType === 'all') {
       return true;
     }
     return recipe.type === filterType.toLowerCase();
@@ -31,21 +31,27 @@ export default function ReceitasFeitas() {
         <button
           data-testid="filter-by-all-btn"
           type="button"
-          onClick={ () => handleFilter('All') }
+          name="all"
+          onClick={ handleFilterClick }
+          disabled={ filterType === 'all' }
         >
           All
         </button>
         <button
           data-testid="filter-by-meal-btn"
           type="button"
-          onClick={ () => handleFilter('Meals') }
+          name="meal"
+          onClick={ handleFilterClick }
+          disabled={ filterType === 'meal' }
         >
           Meals
         </button>
         <button
           data-testid="filter-by-drink-btn"
           type="button"
-          onClick={ () => handleFilter('Drinks') }
+          name="drink"
+          onClick={ handleFilterClick }
+          disabled={ filterType === 'drink' }
         >
           Drinks
         </button>
@@ -58,15 +64,35 @@ export default function ReceitasFeitas() {
                 data-testid={ `${index}-horizontal-image` }
                 src={ recipe.image }
                 alt={ recipe.name }
+                width="200"
               />
-
-              <p data-testid={ `${index}-horizontal-top-text` }>
-                {recipe.nationality ? `${recipe.nationality} - ` : ''}
-                {recipe.category}
-              </p>
 
               <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
             </Link>
+
+            {recipe.type === 'meal' ? (
+              <div>
+                <p data-testid={ `${index}-horizontal-top-text` }>
+                  {recipe.nationality ? `${recipe.nationality} - ` : ''}
+                  {recipe.category}
+                </p>
+
+                {recipe.tags.slice(0, 2).map((tag) => (
+                  <span
+                    key={ tag }
+                    data-testid={ `${index}-${tag}-horizontal-tag` }
+                  >
+                    {tag}
+                  </span>
+                ))}
+
+              </div>
+
+            ) : (
+              <p data-testid={ `${index}-horizontal-top-text` }>
+                {recipe.alcoholicOrNot}
+              </p>
+            )}
 
             <p data-testid={ `${index}-horizontal-done-date` }>
               Done on:
@@ -75,22 +101,15 @@ export default function ReceitasFeitas() {
             </p>
 
             <button
-              data-testid={ `${index}-horizontal-share-btn` }
               type="button"
             >
-              Share
+              <img
+                data-testid={ `${index}-horizontal-share-btn` }
+                src={ shareIcon }
+                alt="Share recipe"
+              />
             </button>
 
-            <div>
-              {recipe.tags.map((tag) => (
-                <span
-                  key={ tag }
-                  data-testid={ `${index}-${tag}-horizontal-tag` }
-                >
-                  {tag}
-                </span>
-              ))}
-            </div>
           </div>
         ))}
       </div>
